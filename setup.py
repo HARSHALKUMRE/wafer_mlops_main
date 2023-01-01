7eeadb39b41a29b7e017ce37ba4eb325630b14ad
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-01-01 09:02:47 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a wafer project base on mlops',
+    author='Harshal Kumre',
+    license='MIT',
+)
